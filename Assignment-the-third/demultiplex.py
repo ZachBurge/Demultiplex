2d76758110c347f47894750f808791d5ede0eba6
ZachBurge <zburge200@gmail.com>
2026-08-03 22:36:15 -0700

    Heatmap created, trying to get better resolution

--- Assignment-the-third/demultiplex.py
+++ Assignment-the-third/demultiplex.py
@@ -154,4 +154,4 @@ plt.xlabel("Forward indexes")
 plt.ylabel("Reverse indexes (rev comp)")
 plt.title("Matched and Hopped Index Pair Counts")
 
-plt.savefig("index_counts_heatmap.png")
+plt.savefig("index_counts_heatmap.png", dpi=300)
