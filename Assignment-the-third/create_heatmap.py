c089401efc6c4449a5d62cc7500bb5c1f525c7c9
ZachBurge <zburge200@gmail.com>
2026-08-04 07:12:54 -0700

    Heatmap done

--- Assignment-the-third/create_heatmap.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-import demultiplex
-
-plt_data = [demultiplex.known_indexes, demultiplex.known_indexes]
-print(plt_data)
