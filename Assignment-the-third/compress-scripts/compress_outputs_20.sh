91ff7d8cc38be92f0b44b76aa82bf09047e8b706
ZachBurge <zburge200@gmail.com>
2026-08-06 11:34:03 -0700

    Compressing output files in /scratch, ran for cutoff 0 as well

---
+++ Assignment-the-third/compress-scripts/compress_outputs_20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH --constraint=turin                # use new nodes
+#SBATCH --account=bgmp                    # REQUIRED: which account to use
+#SBATCH --partition=bgmp                  # REQUIRED: which partition to use
+#SBATCH --cpus-per-task=8                 # optional: number of cpus, default is 1
+
+/usr/bin/time -v pigz -p 8 /scratch/bgmp/zburge/demux/cutoff-20/*.fastq
